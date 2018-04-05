06f8c193c8f5ff721276e9c397a86d3443c6653f
moabe <mrov@cin.ufpe.br>
2018-04-05 06:42:16 -0300

    Cover section add and some file refatoring

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './Header.jsx';
+import CoverSection from './Cover.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <CoverSection/>
+        <button className="btn btn-primary">asdwasdw</button>
+      </div>
+    );
+  }
+}
+
+export default App;
