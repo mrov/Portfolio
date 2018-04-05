a1896f238ab5c41b44606c140aed6d60d7302cde
moabe <mrov@cin.ufpe.br>
2018-04-05 06:08:23 -0300

    Start commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter , Switch , Route } from 'react-router-dom'
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render( 
+  <BrowserRouter>
+    <Switch>
+        <Route exact path= "/" component={App} />
+    </Switch>
+  </BrowserRouter>, document.getElementById('root'));
+registerServiceWorker();
